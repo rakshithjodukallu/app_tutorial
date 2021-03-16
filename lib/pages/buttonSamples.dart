93681d48263e204a1c8a32cf6f11439f3798da58
Rakshith J <62825426+rakshithjodukallu@users.noreply.github.com>
2021-03-16 16:47:34 +0530

    flatbuttons

--- lib/pages/buttonSamples.dart
+++ lib/pages/buttonSamples.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 class ButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: FlatButton(
+          onPressed: () {
+            print("clicked");
+          },
+          onLongPress: () {
+            print("long clicked");
+          },
+          child: Text("click me"),
+          color: Colors.blue,
+          textColor: Colors.white,
+          highlightColor: Colors.green,
+          splashColor: Colors.green,
+          // shape: CircleBorder(),
+          shape: StadiumBorder(),
+          padding: EdgeInsets.all(10.0),
+        ),
+      ),
+    );
   }
 }
